3b416e47894f7edb7ea97cc62b8baa9684096977
Simone Gotti <simone.gotti@gmail.com>
2023-12-13 13:33:18 +0100

    testutil: add detailed errors for zerolog output in tests
    read from the DETAILED_ERRORS env var and if true enable detailed
    errors for zerolog logging in tests.

--- internal/testutil/log.go
+++ internal/testutil/log.go
@@ -2,10 +2,13 @@ package testutil
 
 import (
 	"bytes"
+	"os"
+	"strconv"
 	"testing"
 	"time"
 
 	"github.com/rs/zerolog"
+	"github.com/sorintlab/errors"
 )
 
 // testingWriter is a WriteSyncer that writes to the given testing.TB.
@@ -30,5 +33,19 @@ func (w *testingWriter) Write(p []byte) (n int, err error) {
 }
 
 func NewLogger(t *testing.T) zerolog.Logger {
-	return zerolog.New(zerolog.ConsoleWriter{Out: NewTestingWriter(t), TimeFormat: time.RFC3339Nano}).With().Timestamp().Caller().Logger().Level(zerolog.InfoLevel)
+	detailedErrors, _ := strconv.ParseBool(os.Getenv("DETAILED_ERRORS"))
+
+	if detailedErrors {
+		zerolog.ErrorMarshalFunc = errors.ErrorMarshalFunc
+	}
+
+	cw := zerolog.ConsoleWriter{
+		Out:                 NewTestingWriter(t),
+		TimeFormat:          time.RFC3339Nano,
+		FormatErrFieldValue: errors.FormatErrFieldValue,
+	}
+
+	zerolog.TimeFieldFormat = time.RFC3339Nano
+
+	return zerolog.New(cw).With().Timestamp().Stack().Caller().Logger().Level(zerolog.InfoLevel).Output(cw)
 }
